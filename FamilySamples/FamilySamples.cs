3f8d8214b609559cb4195a5b9551002e65d2374c
aurelijus <aurelijus@AURELIJUSPC>
2016-10-14 17:02:23 +0300

    New Project FamilySamples added

---
+++ FamilySamples/FamilySamples.cs
@@ -0,0 +1,87 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Structure;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilySamples
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    public class FamilySamples : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData revit,
+           ref string message, ElementSet elements)
+        {
+            var doc = revit.Application.ActiveUIDocument.Document;
+
+            var familyPath = @"C:\Users\aurelijus\Documents\Revit\RevitAPISamples\Libraries\Metric\HD200.rfa";
+            var symbolName = "HD200";
+
+            using (var tran = new Transaction(doc, "FamilySamples"))
+            {
+                try
+                {
+                    tran.Start();
+
+                    Family family;
+
+                    if (!doc.LoadFamily(familyPath, out family))
+                    {
+                        throw new Exception("Unable to load family: " + familyPath);
+                    }
+
+                    var symbolIds = family.GetFamilySymbolIds();
+
+                    var symbols = new List<Element>();
+                    foreach (var id in symbolIds)
+                    {
+                        symbols.Add(doc.GetElement(id));
+                    }
+
+                    var symbol = (FamilySymbol)symbols.Where(s => s.Name == symbolName).First();
+
+                    if (!symbol.IsActive)
+                    {
+                        symbol.Activate();
+                    }
+
+
+                    var floorTypeId = doc.GetDefaultFamilyTypeId(new ElementId(BuiltInCategory.OST_Floors));
+                    var floorType = (FloorType)doc.GetElement(floorTypeId);
+
+                    var profile = new CurveArray();
+                    profile.Append(Line.CreateBound(new XYZ(0, 0, 0), new XYZ(0, 50, 0)));
+                    profile.Append(Line.CreateBound(new XYZ(0, 50, 0), new XYZ(50, 50, 0)));
+                    profile.Append(Line.CreateBound(new XYZ(50, 50, 0), new XYZ(50, 0, 0)));
+                    profile.Append(Line.CreateBound(new XYZ(50, 0, 0), new XYZ(0, 0, 0)));
+
+                    var collector = new FilteredElementCollector(doc);
+
+                    var level = (Level)collector.OfClass(typeof(Level)).ToElements().First();
+
+                    var floor = doc.Create.NewFloor(profile, floorType, level, false);
+
+                    var familyInstance = doc.Create.NewFamilyInstance(XYZ.Zero, symbol, XYZ.BasisY, floor, StructuralType.NonStructural);
+
+
+                    tran.Commit();
+                }
+                catch (Exception e)
+                {
+                    if (tran.GetStatus() == TransactionStatus.Started)
+                    {
+                        tran.RollBack();
+                    }
+
+                    System.Windows.MessageBox.Show("Error: " + e.Message);
+                }
+
+            }
+
+            return Result.Succeeded;
+        }
+    }
+}
